c5c52756efdc349b5425a3fdf0c0b9f5ab38e770
Esteban <visualdev29@gmail.com>
2025-08-12 22:25:46 -0600

    Fav-Song-Base

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Song from './components/Song';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Song from "./components/Song";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    songs: [
+      { id: 1, title: "Believer", artist: "Imagine Dragons", album: "Evolve", duration: "3:24" },
+      { id: 2, title: "Yellow", artist: "Coldplay", album: "Parachutes", duration: "4:29" },
+      { id: 3, title: "Fiesta Pagana", artist: "Mägo de Oz", album: "Finisterra", duration: "5:48" },
+      { id: 4, title: "El Poeta", artist: "Humbe", album: "Entropía", duration: "3:28" },
+      { id: 5, title: "Horizonte", artist: "Anthres", album: "Ecos", duration: "3:15" }
+    ]
+  };
+
+  componentDidMount() {
+    console.log("Fav Song se ha cargado correctamente!");
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Header title="Fav Song" />
+        <main className="content">
+          <section className="song-list">
+            {this.state.songs.map(s => (
+              <Song
+                key={s.id}
+                title={s.title}
+                artist={s.artist}
+                album={s.album}
+                duration={s.duration}
+              />
+            ))}
+          </section>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,14 @@
-const Header = () => {
-    return (<></>)
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return (
+            <header className="app-header">
+                <h1>{this.props.title}</h1>
+                <p className="subtitle">Explora y guarda tus canciones</p>
+            </header>
+        );
+    }
 }
 
 export default Header;
